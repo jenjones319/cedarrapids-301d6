a7fe2fba7371ee5ae181a76e3bb53d14ec087fe6
Marc Hauschildt <marc.hauschildt@gmail.com>
2020-07-15 16:40:25 -0500

    Prepped class 2 demo

--- class-02/demo/events-basics/app.js
+++ class-02/demo/events-basics/app.js
@@ -1,15 +1,2 @@
 'use strict';
 
-$('document').ready( function() {
-  $('button').on('click', function() {
-    // Why are we using siblings instead of just targeting 'ul'
-    $(this).siblings('ul').toggleClass('on');
-  });
-
-  $('ul').on('click', 'li', function() {
-    $(this).fadeOut(400);
-    // Would this work? Why?/Why Not?
-    // console.log(this.text());
-  });
-});
-
