a947da1426dc2db7ab995203c1022a64c8c0c4a2
Marc Hauschildt <marc.hauschildt@gmail.com>
2020-07-15 18:55:43 -0500

    Setup class 2 demo jquery-diy

--- class-02/demo/jquery-diy/app-jquery.js
+++ class-02/demo/jquery-diy/app-jquery.js
@@ -9,17 +9,17 @@ $(document).ready(function () {
   $.ajax('./stuff.json', ajaxSettings)
     .then(data => {
       data.people.forEach(person => {
-        $('ul').append(`<li>${person.name}</li>`);
-        /*
-        Use jQuery "clone" to create an object and use methods to set it up, rather than a template literal
-        This is more verbose, but maybe more "correct"
+        $('ul').append(`<li>${person.name} ${person.age}</li>`);
+        
+        // Use jQuery "clone" to create an object and use methods to set it up, rather than a template literal
+        // This is more verbose, but maybe more "correct"
 
-        let $tpl = $('<li></li>').clone();
-        $tpl.html( $('#personTemplate').html() );
-        $tpl.find('.name').text(person.name);
-        $tpl.find('.age').text(person.age);
-        $('ul').append($tpl);
-         */
+        // let $tpl = $('<li></li>').clone();
+        // $tpl.html( $('#personTemplate').html() );
+        // $tpl.find('.name').text(person.name);
+        // $tpl.find('.age').text(person.age);
+        // $('ul').append($tpl);
+        
       });
     });
 
@@ -27,16 +27,15 @@ $(document).ready(function () {
     console.log($(this).text());
   });
 
-  $('div').on('click', function() {
-    $(this).css('background', 'green');
-    console.log($(this).css('background'));
-  });
-
   $('select').on('change', function() {
-    $('div').slideUp();
+    $('div').hide();
     let selectedValue = $(this).val();
     $(`div[data-num=${selectedValue}`).fadeIn();
   });
 
+  $('div').on('click', function() {
+    $(this).css('background', 'green');
+    console.log($(this).css('background'));
+  });
 
 });
